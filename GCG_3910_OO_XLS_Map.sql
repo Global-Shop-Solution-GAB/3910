1c79006d7391d25db9f902dd7b19df6a7992836e
Service Web <sw@gssmail.com>
2024-03-11 08:53:22 -0500

    Commit changes in C:\Websites\Git\emarlow\3910\GCG_3910_OO_XLS_Map.sql

---
+++ GCG_3910_OO_XLS_Map.sql
@@ -0,0 +1,9 @@
+/* ==========================================
+ * TABLE: GCG_3910_OO_XLS_Map
+ * ========================================== */
+CREATE TABLE "GCG_3910_OO_XLS_Map"(
+ "Column_Header" CHAR(50),
+ "Code_Location" CHAR(2),
+ "Code_PL" CHAR(2),
+ "Loc_Desc" CHAR(50),
+ "ID" IDENTITY);
